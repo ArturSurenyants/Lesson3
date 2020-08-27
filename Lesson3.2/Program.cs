3b6163ed2658f54e9a4e94bb812951bcebcb6656
Artur Surenyants <artur.surenyants@gmail.com>
2020-08-27 23:50:00 +0400

    Commit

---
+++ Lesson3.2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float f = 25.2f;
+            float f3 = f + 22.4f;
+
+            Console.WriteLine(f3);
+
+            float f4 = f - 22.4f;
+
+            Console.WriteLine(f4);
+
+            float f5 = f * 22.4f;
+
+            Console.WriteLine(f5);
+
+            float f6 = f + 22.4f;
+
+            Console.WriteLine(f6);
+
+            Console.ReadKey();
+        }
+    }
+}
