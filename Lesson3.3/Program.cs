d3db5f5f76887fabe8edcbda9ef2367a32a11bde
Artur Surenyants <artur.surenyants@gmail.com>
2020-08-28 00:16:23 +0400

    Commit

---
+++ Lesson3.3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson3._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte n1 = 72;
+            byte n2 = n1;
+
+            int n3 = 260;
+            checked
+            {
+               unchecked
+                {
+                    byte n4 = (byte)n3;
+
+                    Console.WriteLine(n4);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
