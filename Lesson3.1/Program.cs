d9108058df897855fe323f09ed496d93f9e2c2e8
Artur Surenyants <artur.surenyants@gmail.com>
2020-08-27 21:30:59 +0400

    Commite

---
+++ Lesson3.1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 68;
+            int n2 = 43;
+            int res = 0;
+
+            res = n1 + n2;
+            Console.WriteLine(res);
+
+            res = n1 - n2;
+            Console.WriteLine(res);
+
+            res = n1 * n2;
+            Console.WriteLine(res);
+
+            res = n1 / n2;
+            Console.WriteLine(res);
+
+            res = n1 % n2;
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
